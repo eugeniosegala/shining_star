7a2abae9c892d17d16bfc10b375489ae43e82a23
Your Name <eugenio.segala@hotmail.it>
2017-10-31 09:29:01 +0100

    rev

--- js/shining_star.js
+++ js/shining_star.js
@@ -8,33 +8,18 @@
  * --------------------------------------------------------------------------*/
 
 
-/**
- * Get a random floating point number between `min` and `max`.
- *
- * @param {number} min - min number
- * @param {number} max - max number
- * @return {number} a random floating point number
- */
 
 function getRandomFloat(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-/**
- * Get a random integer between `min` and `max`.
- *
- * @param {number} min - min number
- * @param {number} max - max number
- * @return {number} a random integer
- */
+
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 
-
-
-function shining_star(selector) {
+function shining_star(selector,stars,diffusion) {
 
   var width = document.querySelector(selector).offsetWidth;
   var height = document.querySelector(selector).offsetHeight;
@@ -46,12 +31,10 @@ function shining_star(selector) {
   var i = 0;
 
 
-
-  for (i = 0; i < 1000; i++) {
-
+  for (i = 0; i < stars; i++) {
 
     /*
-          setTimeout(function(){
+        setTimeout(function(){
 
           document.querySelector(selector).innerHTML += '<div style="animation: blinker ' + getRandomFloat(3, 5) + 's linear infinite; position: absolute; top: ' + getRandomInt(0, height) + '; left: ' + getRandomInt(0, width) + ';" class="blink enter-star"><img width="' + getRandomInt(1, 3) +
             '" src="stella.svg"></div>';
@@ -61,15 +44,16 @@ function shining_star(selector) {
 
     setTimeout(function() {
 
-      $(selector).append('<div style="animation: blinker ' + getRandomFloat(3, 5) + 's linear infinite; position: absolute; top: ' + getRandomInt(0, height) + 'px; left: ' + getRandomInt(0, width) +
+      $(selector).append('<div style="pointer-events: none; animation: blinker ' + getRandomFloat(3, 5) + 's linear infinite; position: absolute; top: ' + getRandomInt(0, height) + 'px; left: ' + getRandomInt(0, width) +
         'px;" class="blink enter-star"><img class="fading" width="' + getRandomInt(1, 2) +
         '" src="img/stella.svg"></div>');
 
+      //  console.log(1);
 
-    //  console.log(1);
+    }, (i * diffusion));
 
-    }, (i * 20));
     //   }, (i * 50) );
+
   }
 
 
